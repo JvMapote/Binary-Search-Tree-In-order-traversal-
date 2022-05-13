e5df3f380255d91332e29c3fc0ea60fb19a4ca26
Jayvee N Mapote <101295973+JvMapote@users.noreply.github.com>
2022-05-13 15:21:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def inorder(node):
+    if node is None:
+        return
+    inorder(node.left)
+    print(node.val)
+    inorder(node.right)
+
+
+root = Node(10)
+root.left = Node(5)
+root.right = Node(15)
+root.left.left = Node(2)
+root.left.right = Node(7)
+root.right.right = Node(20)
+
+inorder(root)
+
